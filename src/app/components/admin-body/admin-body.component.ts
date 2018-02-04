62a5d93e9adcff642f1f734400bdeac38d92f485
kerrongordon <kgpsounds.com@gmail.com>
2018-02-04 18:46:47 -0400

    added admin body

---
+++ src/app/components/admin-body/admin-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'kgp-admin-body',
+  templateUrl: './admin-body.component.html',
+  styleUrls: ['./admin-body.component.sass']
+})
+export class AdminBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
