194e3ed5e664873fdef5f4005fe08c9f56a13f41
kerrongordon <kgpsounds.com@gmail.com>
2018-02-06 03:51:38 -0400

    fix project menu bug

--- src/app/components/admin-bar/admin-bar.component.ts
+++ src/app/components/admin-bar/admin-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { AuthService } from '@services/auth/auth.service'
 
 @Component({
@@ -7,18 +7,29 @@ import { AuthService } from '@services/auth/auth.service'
   styleUrls: ['./admin-bar.component.sass'],
   providers: [AuthService]
 })
-export class AdminBarComponent {
-
-  addminbar = [
-    { title: 'Dashboard', icon: 'ion-ios-analytics-outline', link: '/admin' },
-    { title: 'Message', icon: 'ion-ios-chatboxes-outline', link: '/message' },
-    { title: 'Add Project', icon: 'ion-ios-compose-outline', link: '/addproject' },
-    { title: 'Projects', icon: 'ion-ios-albums-outline', link: '/manageprojects' }
-  ]
+export class AdminBarComponent implements OnChanges {
+  @Input() link = '/manageprojects/all'
+  addminbar: { title: string; icon: string; link: string; }[]
 
   constructor(
     private _as: AuthService
-  ) { }
+  ) {
+    this.addminbar = [
+      { title: 'Dashboard', icon: 'ion-ios-analytics-outline', link: '/admin' },
+      { title: 'Message', icon: 'ion-ios-chatboxes-outline', link: '/message' },
+      { title: 'Add Project', icon: 'ion-ios-compose-outline', link: '/addproject' },
+      { title: 'Projects', icon: 'ion-ios-albums-outline', link: this.link }
+    ]
+   }
+
+  ngOnChanges() {
+    this.addminbar = [
+      { title: 'Dashboard', icon: 'ion-ios-analytics-outline', link: '/admin' },
+      { title: 'Message', icon: 'ion-ios-chatboxes-outline', link: '/message' },
+      { title: 'Add Project', icon: 'ion-ios-compose-outline', link: '/addproject' },
+      { title: 'Projects', icon: 'ion-ios-albums-outline', link: this.link }
+    ]
+  }
 
   logout() {
     return this._as.logout()
