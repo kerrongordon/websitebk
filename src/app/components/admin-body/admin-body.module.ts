defe99fa866428b992b944c93a3aa075cfa1fee6
kerrongordon <kgpsounds.com@gmail.com>
2018-02-10 09:51:20 -0400

    update

--- src/app/components/admin-body/admin-body.module.ts
+++ src/app/components/admin-body/admin-body.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common'
 import { AdminBodyComponent } from './admin-body.component'
 import { FormsModule } from '@angular/forms'
 import { AutosizeModule } from '@directives/autosize/autosize.module'
-import { ButtonModule } from '@components/button/button.module'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    AutosizeModule,
-    ButtonModule
+    AutosizeModule
   ],
   declarations: [AdminBodyComponent],
   exports: [AdminBodyComponent]
